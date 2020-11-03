5c3cf95b7fb9942f9b112b7c1904652ec4a72e55
Aparna Rajeev <57600509+parna86@users.noreply.github.com>
2020-11-04 00:50:20 +0400

    made new function for the requests

--- App.java
+++ App.java
@@ -5,8 +5,6 @@ import java.net.URI;
 import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
-import java.util.List;
-
 import com.google.gson.Gson;
 
 import java.lang.reflect.Type;
@@ -16,7 +14,8 @@ import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.geojson.Polygon;
-import com.mapbox.turf.*;
+import com.mapbox.turf.TurfJoins;
+import com.mapbox.turf.TurfMeasurement;
 
 /**
  * So far, we can send a get request to the required url and get the air quality data as response. Now to create the required files.
@@ -25,7 +24,9 @@ import com.mapbox.turf.*;
  * Run WebServer: java -jar WebServerLite.jar
  * Run aqmaps: java -jar aqmaps-0.0.1-SNAPSHOT.jar 15 06 2021 55.9444 -3.1878 5678 80
  * Turf documentation : https://docs.mapbox.com/android/java/overview/turf/#working-with-geojson
- */
+ * 						https://docs.mapbox.com/archive/android/java/api/libjava-turf/4.0.0/com/mapbox/turf/TurfJoins.html
+ * 
+ */						
 class Coordinate{
 	double lng;
 	double lat;
@@ -63,34 +64,17 @@ public class App
     	 * args[6] = port
     	 */
     	
-        HttpClient client = HttpClient.newHttpClient();
+    	String AQuri = "http://localhost:" + args[6] + "/maps/" + args[2] + "/" + args[1] + "/" + args[0] + "/air-quality-data.json";
+    	String BDuri = "http://localhost:" + args[6] + "/buildings/no-fly-zones.geojson";
         
-        /**
-         * This is the request for the air quality data
-         */
-        HttpRequest requestAQ = HttpRequest.newBuilder()
-              .uri(URI.create("http://localhost:" + args[6] + "/maps/" + args[2] + "/" + args[1] + "/" + args[0] + "/air-quality-data.json"))
-              .header("Content-type", "application/json")
-              .build();
-        HttpResponse<String> responseAQ = client.send(requestAQ, BodyHandlers.ofString());
-        
+    	String currReadings = sendHTTP(AQuri);
+    	String noFlyZones = sendHTTP(BDuri);
+    	
+    	
         double startingLat = Double.parseDouble(args[3]);
         double startingLong = Double.parseDouble(args[4]);
+        Point start = Point.fromLngLat(startingLong, startingLat);
         
-        /**
-         * The request for the no-fly zones
-         */
-        HttpRequest requestBD = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:" + args[6] + "/buildings/no-fly-zones.geojson"))
-                .header("Content-type", "application/json")
-                .build();
-        
-        HttpResponse<String> responseBD = client.send(requestBD, BodyHandlers.ofString());
-        
-        String currReadings = responseAQ.body();
-        String noFlyZones = responseBD.body();
-//        System.out.println(currReadings);
-//        System.out.println(noFlyZones);
         
         //this is for finding the actual coordinates for each of the ww3 strings - storing data in SensorReadings object only
         Type listType = new TypeToken<ArrayList<SensorReadings>>() {}.getType();
@@ -100,14 +84,19 @@ public class App
         }
         
         //below - extracting each of the coordinates for the buildings in a list of list of points.
-//        Feature f = Feature.fromGeometry(buildings.features().get(0).geometry());
+        
         FeatureCollection buildings = FeatureCollection.fromJson(noFlyZones.toString());
-        List<List<Point>> points = new ArrayList<List<Point>>();
-        for(Feature one : buildings.features()) {
-        	Polygon sample = (Polygon) one.geometry();
-        	points.add(sample.coordinates().get(0));
-        }
-        //cant use awt Polygon as that only accepts ints yay
+        
+        Feature f = Feature.fromGeometry(buildings.features().get(0).geometry());
+        Polygon sample = (Polygon) f.geometry();
+        
+//        List<List<Point>> points = new ArrayList<List<Point>>();
+//        for(Feature one : buildings.features()) {
+//        	Polygon sample = (Polygon) one.geometry();
+//        	points.add(sample.coordinates().get(0));
+//        }
+        
+        System.out.println(TurfJoins.inside(start, sample));
         
     }
     
@@ -119,19 +108,29 @@ public class App
      * @throws InterruptedException
      */
     public static Coordinate convertCoord(String threewords) throws IOException, InterruptedException {
-    	HttpClient client = HttpClient.newHttpClient();
-    	
     	String words[] = threewords.split("\\.");
-    	HttpRequest requestCoord = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:80/words/" + words[0] + "/" + words[1]+ "/" + words[2] + "/details.json"))
+    	String ww3uri = "http://localhost:80/words/" + words[0] + "/" + words[1]+ "/" + words[2] + "/details.json";
+    	String coord =  sendHTTP(ww3uri);
+    	
+    	var sensorCoord = new Gson().fromJson(coord.toString(), ww3ToCoord.class);
+    	return sensorCoord.coordinates;
+    }
+    
+    /***
+     * 
+     * @param uri: uri required to send request
+     * @return String response to the HTTP request
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static String sendHTTP(String uri) throws IOException, InterruptedException {
+    	HttpClient client = HttpClient.newHttpClient();
+    	HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
                 .header("Content-type", "application/json")
                 .build();
-    	
-    	HttpResponse<String> coord = client.send(requestCoord, BodyHandlers.ofString());
-    	
-    	var sensorCoord = new Gson().fromJson(coord.body().toString(), ww3ToCoord.class);
-    	
-    	return sensorCoord.coordinates;
+          HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+          return response.body();
     }
 }
 
