3dd0ee943b86f15dacc45732aa9f9c7cd2868988
Aparna Rajeev <57600509+parna86@users.noreply.github.com>
2020-11-12 01:54:35 +0400

    Only sensor reading algo left

--- App.java
+++ App.java
@@ -5,9 +5,15 @@ import java.net.URI;
 import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 import com.google.gson.Gson;
 
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.MathContext;
 
 import com.google.gson.reflect.TypeToken;
 import com.mapbox.geojson.Feature;
@@ -18,14 +24,14 @@ import com.mapbox.turf.TurfJoins;
 import com.mapbox.turf.TurfMeasurement;
 
 /**
- * So far, we can send a get request to the required url and get the air quality data as response. Now to create the required files.
- * The air quality data is stored in currReadings. Need to use the other command line arguments. 
- * 
+ * Things to keep in hand
  * Run WebServer: java -jar WebServerLite.jar
  * Run aqmaps: java -jar aqmaps-0.0.1-SNAPSHOT.jar 15 06 2021 55.9444 -3.1878 5678 80
  * Turf documentation : https://docs.mapbox.com/android/java/overview/turf/#working-with-geojson
  * 						https://docs.mapbox.com/archive/android/java/api/libjava-turf/4.0.0/com/mapbox/turf/TurfJoins.html
- * 
+ * To calculate direction of one coordinate: https://docs.oracle.com/javase/7/docs/api/java/lang/Math.html#atan2%28double,%20double%29
+ * "From (𝑥1,𝑦1) to (𝑥2,𝑦2) the direction is atan2(𝑦2−𝑦1,𝑥2−𝑥1) - You wrote you were writing a program, so atan2 
+ * is the easiest solution, it's usually found in your languages math package. In java it's Math.atan2(y, x)"
  */						
 class Coordinate{
 	double lng;
@@ -64,6 +70,16 @@ public class App
     	 * args[6] = port
     	 */
     	
+    	/*
+    	 * Below are the borders for the map
+    	 */
+    	final var long_W = new BigDecimal(-3.192473);
+    	final var long_E = new BigDecimal(-3.184319);
+    	final var lat_N = new BigDecimal(55.946233);
+    	final var lat_S = new BigDecimal(55.942617);
+    	Polygon map = Polygon.fromLngLats(constructMap(lat_N, lat_S, long_E, long_W));
+    	
+    	
     	String AQuri = "http://localhost:" + args[6] + "/maps/" + args[2] + "/" + args[1] + "/" + args[0] + "/air-quality-data.json";
     	String BDuri = "http://localhost:" + args[6] + "/buildings/no-fly-zones.geojson";
         
@@ -73,31 +89,88 @@ public class App
     	
         double startingLat = Double.parseDouble(args[3]);
         double startingLong = Double.parseDouble(args[4]);
-        Point start = Point.fromLngLat(startingLong, startingLat);
+        var start = Point.fromLngLat(startingLong, startingLat);
         
         
         //this is for finding the actual coordinates for each of the ww3 strings - storing data in SensorReadings object only
         Type listType = new TypeToken<ArrayList<SensorReadings>>() {}.getType();
         ArrayList<SensorReadings> readings = new Gson().fromJson(currReadings, listType);
         for(SensorReadings one : readings) {
-        	one.coordinates = convertCoord(one.location);
+        	one.coordinates = convertCoord(one.location); //these are all the readings from one day 
         }
         
         //below - extracting each of the coordinates for the buildings in a list of list of points.
         
-        FeatureCollection buildings = FeatureCollection.fromJson(noFlyZones.toString());
+        FeatureCollection bldgs = FeatureCollection.fromJson(noFlyZones.toString());
         
-        Feature f = Feature.fromGeometry(buildings.features().get(0).geometry());
-        Polygon sample = (Polygon) f.geometry();
-        
-//        List<List<Point>> points = new ArrayList<List<Point>>();
-//        for(Feature one : buildings.features()) {
-//        	Polygon sample = (Polygon) one.geometry();
-//        	points.add(sample.coordinates().get(0));
-//        }
-        
-        System.out.println(TurfJoins.inside(start, sample));
-        
+        ArrayList<Feature> f = new ArrayList<Feature>(bldgs.features());
+        Map<String, Polygon> buildings = new HashMap<String, Polygon>();
+        /*
+         * buildings is a hashmap that stores the polygon that represents each obstacle.  
+         */
+        for(Feature feature : f) {
+        	String name = feature.properties().get("name").toString();
+        	buildings.put(name.substring(1, name.length()-1), (Polygon)feature.geometry());
+        }
+        /*MAIN ALGO */
+        String move = "";
+        var moveCount = 0;
+        while(moveCount <= 150) {
+        	BigDecimal minDist = new BigDecimal("-1");
+        	int closestIndex = -1;
+        	for(SensorReadings one: readings) {
+        		BigDecimal[] currPoint = {new BigDecimal(start.latitude()), new BigDecimal(start.longitude())};
+        		BigDecimal[] sensorPoint = {new BigDecimal(one.coordinates.lat), new BigDecimal(one.coordinates.lng)};
+        		var currDist = euclidDist(currPoint, sensorPoint);
+        		System.out.println("Distance calculated by my function:" + currDist);
+        		System.out.println("minDist val: " + minDist);
+        		System.out.println("------");
+        		System.out.println("Starting point:" + currPoint[0]);
+        		System.out.println("Sensor Point: " + sensorPoint[0]);
+        		if(minDist.compareTo(currDist) == 1) {
+        			//if minDist > currDist
+        			minDist = currDist;
+        			closestIndex = readings.indexOf(one);
+        			System.out.println("Closest point updated to idx " + closestIndex);
+        		}
+        	}
+        	System.out.println(closestIndex);
+        	BigDecimal[] closestPoint = {new BigDecimal(readings.get(closestIndex).coordinates.lat), new BigDecimal(readings.get(closestIndex).coordinates.lng)};
+        	
+        	//would've found potentially closest sensor
+        	//now calculate the direction of this point
+//        	var direction = Math.atan2(closestPoint[1].subtract(), x)
+        	moveCount= moveCount+500;
+        }
+    }
+    /***
+     * 
+     * @param north
+     * @param south
+     * @param east
+     * @param west
+     * @return temp2 - a list of list of points - for polygons
+     */
+    
+    private static List<List<Point>> constructMap(BigDecimal north, BigDecimal south, BigDecimal east, BigDecimal west){
+    	BigDecimal lat_diff = north.subtract(south);
+    	BigDecimal long_diff = east.subtract(west);
+    	lat_diff = lat_diff.divide(new BigDecimal(10));
+    	long_diff = long_diff.divide(new BigDecimal(10));
+    	
+    	/**
+    	 * ArrayList of coordinates below. Need to create that for individual polygons too.
+    	 */
+    	final var coordinates = new ArrayList<Point>();
+    	coordinates.add(Point.fromLngLat(west.doubleValue(), north.doubleValue()));
+    	coordinates.add(Point.fromLngLat(east.doubleValue(), north.doubleValue()));
+    	coordinates.add(Point.fromLngLat(east.doubleValue(), south.doubleValue()));
+    	coordinates.add(Point.fromLngLat(west.doubleValue(), south.doubleValue()));
+    	coordinates.add(Point.fromLngLat(west.doubleValue(), north.doubleValue()));
+    	
+    	List<List<Point>> temp2 = new ArrayList<List<Point>>();
+    	temp2.add(coordinates);
+    	return temp2;
     }
     
     /***
@@ -107,7 +180,8 @@ public class App
      * @throws IOException
      * @throws InterruptedException
      */
-    public static Coordinate convertCoord(String threewords) throws IOException, InterruptedException {
+    
+    private static Coordinate convertCoord(String threewords) throws IOException, InterruptedException {
     	String words[] = threewords.split("\\.");
     	String ww3uri = "http://localhost:80/words/" + words[0] + "/" + words[1]+ "/" + words[2] + "/details.json";
     	String coord =  sendHTTP(ww3uri);
@@ -123,7 +197,7 @@ public class App
      * @throws IOException
      * @throws InterruptedException
      */
-    public static String sendHTTP(String uri) throws IOException, InterruptedException {
+    private static String sendHTTP(String uri) throws IOException, InterruptedException {
     	HttpClient client = HttpClient.newHttpClient();
     	HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
@@ -131,6 +205,18 @@ public class App
                 .build();
           HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
           return response.body();
+    }
+    
+    /***
+     * @implNote calculates the euclidean distance between two points
+     * @param point1
+     * @param point2
+     * @return distance[] - BigDecimal array that contains sqrt( diffx^2 + diffy^2)
+     */
+    public static BigDecimal euclidDist(BigDecimal[] point1, BigDecimal[] point2) {
+    	BigDecimal distance =  (point1[0].subtract(point2[0])).add(point1[1].subtract(point2[1])).pow(2);
+    	MathContext precision = new MathContext(8);
+    	return distance.sqrt(precision);
     }
 }
 
